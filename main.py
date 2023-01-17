ca9a03ff3318b68e214de2292be28f5051bf5bbe
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-17 08:54:32 -0400

    response_model

--- main.py
+++ main.py
@@ -27,38 +27,53 @@ class Person(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Douglas"
         )
     last_name: str = Field(
         ...,
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Tovar"
     )
     age: int = Field(
         ...,
         gt=0,
-        le=115
+        le=115,
+        example=42
     )
-    hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(default=None)
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
+    password: str = Field(..., min_length=8)
     
-    class Config:
-        schema_extra = {
-            "example": {
-                "first_name": "Douglas",
-                "last_name": "Tovar",
-                "age": 21,
-                "hair_color": "blonde",
-                "is_married": False
-            }
-        }
-
+class PersonOut(BaseModel):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Douglas"
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Tovar"
+    )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115,
+        example=42
+    )
+    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
+    is_married: Optional[bool] = Field(default=None, example=False)
+    
 @app.get("/")
 def home():
     return {"hello":"World"}
 
 #Request and Response Body
-@app.post("/person/new")
+@app.post("/person/new", response_model=PersonOut)
 def create_person(person: Person = Body(...)):
     return person
     
