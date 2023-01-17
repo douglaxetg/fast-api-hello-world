0e0c9390cea5b7142e3d29552484ef3b367f784f
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-17 09:05:11 -0400

    remove_duplicated

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Location(BaseModel):
     state: str
     country: str
 
-class Person(BaseModel):
+class PersonBase(BaseModel):
     first_name: str = Field(
         ...,
         min_length=1,
@@ -44,29 +44,12 @@ class Person(BaseModel):
     )
     hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
     is_married: Optional[bool] = Field(default=None, example=False)
+    
+class Person(PersonBase):
     password: str = Field(..., min_length=8)
     
-class PersonOut(BaseModel):
-    first_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Douglas"
-        )
-    last_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50,
-        example="Tovar"
-    )
-    age: int = Field(
-        ...,
-        gt=0,
-        le=115,
-        example=42
-    )
-    hair_color: Optional[HairColor] = Field(default=None, example=HairColor.black)
-    is_married: Optional[bool] = Field(default=None, example=False)
+class PersonOut(PersonBase):
+    pass
     
 @app.get("/")
 def home():
