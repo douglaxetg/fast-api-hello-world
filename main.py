d546f5673a6e02cf367927b4221400cafa65d3c3
Douglas Tovar <douglastovar.g@gmail.com>
2023-01-17 20:18:26 -0400

    httpexception

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from pydantic import Field
 from pydantic import EmailStr
 
 #FastAPI
-from fastapi import FastAPI, Body, Query, Path, status, Form, Header, Cookie, UploadFile, File
+from fastapi import FastAPI
+from fastapi import status
+from fastapi import HTTPException
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -93,6 +96,8 @@ def show_person(
 ):
     return {name: age}
 
+persons = [1,2,3,4,5]
+
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
@@ -101,6 +106,11 @@ def show_person(
         example=123
         )
 ):
+    if person_id not in persons:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="This person doesn't exist!"
+        )
     return {person_id: "It exist!"}
 
 
